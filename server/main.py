357265b31b4d544a8dce860e9d127310413bceb7
Joe Oak <josephjoak@gmail.com>
2025-08-24 23:10:24 -0700

    Group server script functions

--- server/main.py
+++ server/main.py
@@ -19,40 +19,27 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+DECK = [
+    # fmt: off
+    "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "10H", "JH", "QH", "KH", "AH",
+    "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "10D", "JD", "QD", "KD", "AD",
+    "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "10C", "JC", "QC", "KC", "AC",
+    "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "10S", "JS", "QS", "KS", "AS",
+    # fmt: on
+]
 
-def parse_card(card: str) -> tuple[int, str]:
-    rank = card[:-1]
-    suit = card[-1]
-
-    if rank == "A":
-        rank = "14"
-    elif rank == "K":
-        rank = "13"
-    elif rank == "Q":
-        rank = "12"
-    elif rank == "J":
-        rank = "11"
-
-    return int(rank), suit
-
-
-def is_round_over():
-    for player in players.values():
-        if len(player.hand) > 0:
-            return False
-
-    return True
-
-
-def reset_tricks_taken():
-    for player in players.values():
-        player.tricks = []
+MAX_PLAYERS = 2
 
 
 class GamePhase(Enum):
     WAITING = auto()
     PLAYING = auto()
     GAME_OVER = auto()
+
+
+class PlayerType(Enum):
+    BOT = auto()
+    HUMAN = auto()
 
 
 class Trick(BaseModel):
@@ -63,101 +50,31 @@ class Trick(BaseModel):
     winning_player: str = ""
 
 
-def give_trick_to_player(trick: Trick, session_id: str):
-    player = players.get(session_id)
+class Player(BaseModel):
+    hand: List[str] = []
+    is_winner: bool = False
+    name: str
+    scores: List[int] = []
+    session_id: str
+    total_score: int = 0
+    tricks: List[Trick] = []
+    type: PlayerType
 
-    if player:
-        player.tricks.append(trick)
-
-
-def get_trick_count_penalty(tricks: List[Trick]) -> int:
-    return len(tricks)
-
-
-def get_hearts_penalty(tricks: List[Trick]) -> int:
-    penalty = 0
-
-    for trick in tricks:
-        for card in trick.cards:
-            if parse_card(card)[1] == "H":
-                penalty += 10
-
-    return penalty
-
-
-def get_queens_penalty(tricks: List[Trick]) -> int:
-    penalty = 0
-
-    for trick in tricks:
-        for card in trick.cards:
-            if card in ["QC", "QD", "QH", "QS"]:
-                penalty += 25
-
-    return penalty
+    def to_dict(self) -> Dict[str, Any]:
+        return {
+            "hand": self.hand,
+            "name": self.name,
+            "is_winner": self.is_winner,
+            "scores": self.scores,
+            "session_id": self.session_id,
+            "total_score": self.total_score,
+            "tricks": [trick.model_dump() for trick in self.tricks],
+            "type": self.type.name,
+        }
 
 
-def get_ks_penalty(tricks: List[Trick]) -> int:
-    penalty = 0
-
-    for trick in tricks:
-        if "KS" in trick.cards:
-            penalty += 50
-
-    return penalty
-
-
-def get_last_trick_penalty(tricks: List[Trick]) -> int:
-    penalty = 0
-
-    for trick in tricks:
-        if trick.is_last_trick:
-            penalty += 100
-
-    return penalty
-
-
-def calculate_scores():
-    current_round = game_state.round
-
-    for player in players.values():
-        score = 0
-
-        if current_round == 1:
-            score += get_trick_count_penalty(player.tricks)
-        elif current_round == 2:
-            score += get_hearts_penalty(player.tricks)
-        elif current_round == 3:
-            score += get_queens_penalty(player.tricks)
-        elif current_round == 4:
-            score += get_ks_penalty(player.tricks)
-        elif current_round == 5:
-            score += get_last_trick_penalty(player.tricks)
-        elif current_round == 6:
-            score += get_trick_count_penalty(player.tricks)
-            score += get_hearts_penalty(player.tricks)
-            score += get_queens_penalty(player.tricks)
-            score += get_ks_penalty(player.tricks)
-            score += get_last_trick_penalty(player.tricks)
-
-        player.scores.append(score)
-
-
-def get_winners():
-    total_scores = []
-
-    for player in players.values():
-        total_scores.append((player.session_id, sum(player.scores)))
-
-    lowest_score = min([score for _, score in total_scores])
-
-    for session_id, score in total_scores:
-        if score == lowest_score:
-            players[session_id].is_winner = True
-
-
-class PlayerType(Enum):
-    BOT = auto()
-    HUMAN = auto()
+players: Dict[str, Player] = {}
+session_to_socket: Dict[str, WebSocket] = {}
 
 
 class GameState(BaseModel):
@@ -259,40 +176,6 @@ class GameState(BaseModel):
         }
 
 
-class Player(BaseModel):
-    hand: List[str] = []
-    is_winner: bool = False
-    name: str
-    scores: List[int] = []
-    session_id: str
-    total_score: int = 0
-    tricks: List[Trick] = []
-    type: PlayerType
-
-    def to_dict(self) -> Dict[str, Any]:
-        return {
-            "hand": self.hand,
-            "name": self.name,
-            "is_winner": self.is_winner,
-            "scores": self.scores,
-            "session_id": self.session_id,
-            "total_score": self.total_score,
-            "tricks": [trick.model_dump() for trick in self.tricks],
-            "type": self.type.name,
-        }
-
-
-MAX_PLAYERS = 2
-
-DECK = [
-    # fmt: off
-    "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "10H", "JH", "QH", "KH", "AH",
-    "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "10D", "JD", "QD", "KD", "AD",
-    "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "10C", "JC", "QC", "KC", "AC",
-    "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "10S", "JS", "QS", "KS", "AS",
-    # fmt: on
-]
-
 game_state = GameState(
     current_trick=Trick(),
     deck=DECK.copy(),
@@ -303,9 +186,6 @@ game_state = GameState(
     turn_order=[],
     turn_player="",
 )
-
-players: Dict[str, Player] = {}
-session_to_socket: Dict[str, WebSocket] = {}
 
 
 @app.get("/")
@@ -361,12 +241,7 @@ async def websocket_endpoint(websocket: WebSocket):
             disconnect_player(session_id)
 
 
-def add_player(session_id: str):
-    players[session_id] = Player(
-        name=f"Player {session_id}",
-        session_id=session_id,
-        type=PlayerType.HUMAN,
-    )
+"""WebSockets"""
 
 
 async def broadcast(payload: Dict[str, Any]):
@@ -381,9 +256,107 @@ def connect_player(session_id: str, websocket: WebSocket):
     session_to_socket[session_id] = websocket
 
 
+def disconnect_player(session_id: str):
+    del session_to_socket[session_id]
+
+
+def generate_session_id() -> str:
+    return generate(alphabet="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ", size=4)
+
+
+"""Players"""
+
+
+def add_player(session_id: str):
+    players[session_id] = Player(
+        name=f"Player {session_id}",
+        session_id=session_id,
+        type=PlayerType.HUMAN,
+    )
+
+
+def get_players() -> Dict[str, Player]:
+    return {player.session_id: player.to_dict() for player in players.values()}
+
+
+def give_trick_to_player(trick: Trick, session_id: str):
+    player = players.get(session_id)
+
+    if player:
+        player.tricks.append(trick)
+
+
+def reset_tricks_taken():
+    for player in players.values():
+        player.tricks = []
+
+
+"""Bots"""
+
+
+def add_bot():
+    session_id = generate_session_id()
+
+    players[session_id] = Player(
+        name=f"Bot {session_id}",
+        session_id=session_id,
+        type=PlayerType.BOT,
+    )
+
+
+def is_player_bot(session_id: str) -> bool:
+    player = players.get(session_id)
+
+    if player and player.type == PlayerType.BOT:
+        return True
+
+    return False
+
+
+async def schedule_bot_move(session_id: str):
+    player = players.get(session_id)
+    if not player:
+        return
+
+    leading_suit = game_state.current_trick.leading_suit
+
+    matching_cards = []
+    for card in player.hand:
+        if parse_card(card)[1] == leading_suit:
+            matching_cards.append(card)
+
+    if matching_cards:
+        card_to_play = min(matching_cards, key=parse_card)
+    else:
+        card_to_play = max(player.hand, key=parse_card)
+
+    await asyncio.sleep(0.25)
+    await simulate_message(session_id, card_to_play)
+
+
+async def simulate_message(session_id: str, card: str):
+    await play_card(session_id, card)
+
+
+"""Game state"""
+
+
+def start_game():
+    if len(players) < 4:
+        while len(players) < 4:
+            add_bot()
+
+    game_state.game_phase = GamePhase.PLAYING
+    set_turn_order()
+    game_state.turn_player = game_state.get_turn_player()
+    shuffle_deck()
+    deal_cards()
+
+
 def deal_cards():
     deck = game_state.deck
-    hand_size = 13
+    # hand_size = 13
+    hand_size = 1
 
     for player in players.values():
         player.hand = deck[:hand_size]
@@ -391,62 +364,32 @@ def deal_cards():
         player.hand.sort(key=lambda card: (parse_card(card)[1], parse_card(card)[0]))
 
 
-def disconnect_player(session_id: str):
-    del session_to_socket[session_id]
-
-
-def draw_deck(session_id: str, card: str):
-    player = players.get(session_id)
-    deck = game_state.deck
-
-    if player and card in deck:
-        deck.remove(card)
-        player.hand.append(card)
-
-
-def draw_discard(session_id: str, card: str):
-    player = players.get(session_id)
-    discard_pile = game_state.discard_pile
-
-    if player and card in discard_pile:
-        discard_pile.remove(card)
-        player.hand.append(card)
-
-
-def generate_session_id() -> str:
-    return generate(alphabet="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ", size=4)
-
-
-def get_players() -> Dict[str, Player]:
-    return {player.session_id: player.to_dict() for player in players.values()}
-
-
-def move_card_left(session_id: str, card: str):
-    player = players.get(session_id)
-
-    if player and card in player.hand:
-        current_index = player.hand.index(card)
-        new_index = current_index - 1
-
-        if current_index > 0:
-            player.hand.pop(current_index)
-            player.hand.insert(new_index, card)
-
-
-def move_card_right(session_id: str, card: str):
-    player = players.get(session_id)
-
-    if player and card in player.hand:
-        current_index = player.hand.index(card)
-        new_index = current_index + 1
-
-        if current_index < len(player.hand) - 1:
-            player.hand.pop(current_index)
-            player.hand.insert(new_index, card)
-
-
 def has_suit_in_hand(suit: str, hand: List[str]) -> bool:
     return any(parse_card(card)[1] == suit for card in hand)
+
+
+def is_round_over():
+    for player in players.values():
+        if len(player.hand) > 0:
+            return False
+
+    return True
+
+
+def parse_card(card: str) -> tuple[int, str]:
+    rank = card[:-1]
+    suit = card[-1]
+
+    if rank == "A":
+        rank = "14"
+    elif rank == "K":
+        rank = "13"
+    elif rank == "Q":
+        rank = "12"
+    elif rank == "J":
+        rank = "11"
+
+    return int(rank), suit
 
 
 async def play_card(session_id: str, card: str):
@@ -505,60 +448,92 @@ def shuffle_deck():
     random.shuffle(game_state.deck)
 
 
-def start_game():
-    if len(players) < 4:
-        while len(players) < 4:
-            add_bot()
-
-    game_state.game_phase = GamePhase.PLAYING
-    set_turn_order()
-    game_state.turn_player = game_state.get_turn_player()
-    shuffle_deck()
-    deal_cards()
+"""Scoring"""
 
 
-def add_bot():
-    session_id = generate_session_id()
+def calculate_scores():
+    current_round = game_state.round
 
-    players[session_id] = Player(
-        name=f"Bot {session_id}",
-        session_id=session_id,
-        type=PlayerType.BOT,
-    )
+    for player in players.values():
+        score = 0
 
+        if current_round == 1:
+            score += get_trick_count_penalty(player.tricks)
+        elif current_round == 2:
+            score += get_hearts_penalty(player.tricks)
+        elif current_round == 3:
+            score += get_queens_penalty(player.tricks)
+        elif current_round == 4:
+            score += get_ks_penalty(player.tricks)
+        elif current_round == 5:
+            score += get_last_trick_penalty(player.tricks)
+        elif current_round == 6:
+            score += get_trick_count_penalty(player.tricks)
+            score += get_hearts_penalty(player.tricks)
+            score += get_queens_penalty(player.tricks)
+            score += get_ks_penalty(player.tricks)
+            score += get_last_trick_penalty(player.tricks)
 
-def is_player_bot(session_id: str) -> bool:
-    player = players.get(session_id)
-
-    if player and player.type == PlayerType.BOT:
-        return True
-
-    return False
-
-
-async def schedule_bot_move(session_id: str):
-    player = players.get(session_id)
-    if not player:
-        return
-
-    leading_suit = game_state.current_trick.leading_suit
-
-    matching_cards = []
-    for card in player.hand:
-        if parse_card(card)[1] == leading_suit:
-            matching_cards.append(card)
-
-    if matching_cards:
-        card_to_play = min(matching_cards, key=parse_card)
-    else:
-        card_to_play = max(player.hand, key=parse_card)
-
-    await asyncio.sleep(0.25)
-    await simulate_message(session_id, card_to_play)
+        player.scores.append(score)
 
 
-async def simulate_message(session_id: str, card: str):
-    await play_card(session_id, card)
+def get_trick_count_penalty(tricks: List[Trick]) -> int:
+    return len(tricks)
+
+
+def get_hearts_penalty(tricks: List[Trick]) -> int:
+    penalty = 0
+
+    for trick in tricks:
+        for card in trick.cards:
+            if parse_card(card)[1] == "H":
+                penalty += 10
+
+    return penalty
+
+
+def get_queens_penalty(tricks: List[Trick]) -> int:
+    penalty = 0
+
+    for trick in tricks:
+        for card in trick.cards:
+            if card in ["QC", "QD", "QH", "QS"]:
+                penalty += 25
+
+    return penalty
+
+
+def get_ks_penalty(tricks: List[Trick]) -> int:
+    penalty = 0
+
+    for trick in tricks:
+        if "KS" in trick.cards:
+            penalty += 50
+
+    return penalty
+
+
+def get_last_trick_penalty(tricks: List[Trick]) -> int:
+    penalty = 0
+
+    for trick in tricks:
+        if trick.is_last_trick:
+            penalty += 100
+
+    return penalty
+
+
+def get_winners():
+    total_scores = []
+
+    for player in players.values():
+        total_scores.append((player.session_id, sum(player.scores)))
+
+    lowest_score = min([score for _, score in total_scores])
+
+    for session_id, score in total_scores:
+        if score == lowest_score:
+            players[session_id].is_winner = True
 
 
 if __name__ == "__main__":
